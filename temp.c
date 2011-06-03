843bde17c7acff279b9c869708027733cec75fa8
Michael Moon <triffid.hunter@gmail.com>
2011-06-04 03:15:28 +1000

    merge cleanup, missed a section in temp.c
    Conflicts:
    
    	temp.c

--- temp.c
+++ temp.c
@@ -293,7 +293,7 @@ void temp_sensor_tick() {
 			temp_sensors_runtime[i].last_read_temp = temp;
 		}
 		#ifdef EECONFIG
-			if (labs((int16_t)(temp_sensors_runtime[i].last_read_temp - temp_sensors_runtime[i].target_temp)) < (eeconfig.temp_hysteresis * 4)) {
+			if (labs(temp_sensors_runtime[i].last_read_temp - temp_sensors_runtime[i].target_temp) < eeconfig.temp_hysteresis) {
 				if (temp_sensors_runtime[i].temp_residency < eeconfig.temp_residency)
 					temp_sensors_runtime[i].temp_residency++;
 			}
@@ -301,8 +301,8 @@ void temp_sensor_tick() {
 				temp_sensors_runtime[i].temp_residency = 0;
 			}
 		#else
-			if (labs((int16_t)(temp_sensors_runtime[i].last_read_temp - temp_sensors_runtime[i].target_temp)) < (TEMP_HYSTERESIS*4)) {
-				if (temp_sensors_runtime[i].temp_residency < (TEMP_RESIDENCY_TIME*100))
+			if (labs(temp_sensors_runtime[i].last_read_temp - temp_sensors_runtime[i].target_temp) < (TEMP_HYSTERESIS * 4)) {
+				if (temp_sensors_runtime[i].temp_residency < TEMP_RESIDENCY_TIME*100)
 					temp_sensors_runtime[i].temp_residency++;
 			}
 			else {
